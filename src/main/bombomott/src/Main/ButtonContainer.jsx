b39b2706f44862323dfe6509ce78fe813c718b1b
Woongchan Jeon <jchan71571@gmail.com>
2023-10-01 22:40:29 +0900

    2301001-1

--- src/main/bombomott/src/Main/ButtonContainer.jsx
+++ src/main/bombomott/src/Main/ButtonContainer.jsx
@@ -4,19 +4,6 @@ import buttonContent from './buttonContent.json'
 import './css/buttonContainer.css'
 
 export default function ButtonContainer() {
-    return (
-        <div className='button-area'>
-            <div className="button-container">
-                <ButtonContent/>
-                {/* <video loop autoPlay muted controls>
-                    <source src={`https://vod-bgc-oc-east-1.media.dssott.com/bgui/ps01/disney/bgui/2019/08/01/1564674844-disney.mp4`} type='video/mp4'/>
-                </video> */}
-            </div>
-        </div>
-    );
-}
-
-function ButtonContent() {
     const [mouseIndex, setMouseIndex] = useState('')
     const [isPlaying, setIsPlaying] = useState('autoPlay');
 
@@ -26,14 +13,15 @@ function ButtonContent() {
         }
     }
 
-    return(
-        <>
-        {
+    return (
+        <div className='button-area'>
+            <div className="button-container">
+            {
             buttonContent.data.map((a,i)=>{
                 return(
                     <div className="button-box" key={i}
-                                onMouseEnter={()=>{setMouseIndex(i); console.log(mouseIndex)}} 
-                                onMouseLeave={()=>{setMouseIndex(''); console.log(mouseIndex)}}>
+                            onMouseEnter={()=>{setMouseIndex(i); console.log(mouseIndex)}} 
+                            onMouseLeave={()=>{setMouseIndex(''); console.log(mouseIndex)}}>
                         <div className="button-cover">
                             <img src={`${buttonContent.data[i].img}`}/>
                             <video className='button-video' style={videoStyle(i)} loop autoPlay muted>
@@ -41,10 +29,12 @@ function ButtonContent() {
                             </video>
                         </div>
                     </div>
-                )
-            })
-        }
-        </>
-    )
+                    )
+                })
+            }
+            </div>
+        </div>
+    );
 }
+
 
