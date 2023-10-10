82bd8d4d4602287f6be617d44d07bb71a9890232
gyusikbbang <135415038+gyusikbbang@users.noreply.github.com>
2023-10-10 20:00:23 +0900

    2023-10-10 백엔드 커밋

---
+++ src/main/java/com/example/BomBom/Repository/mybatis/MybatisItemRepository.java
@@ -0,0 +1,40 @@
+package com.example.BomBom.Repository.mybatis;
+
+import com.example.BomBom.Repository.MemberRepository;
+import com.example.BomBom.Repository.MemberSearchC;
+import com.example.BomBom.Repository.MemberUpdateDto;
+
+import com.example.BomBom.domain.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MybatisItemRepository implements MemberRepository {
+
+    private final MemberMapper memberMapper;
+    @Override
+    public Member save(Member member) {
+        memberMapper.save(member);
+        return member;
+    }
+
+    @Override
+    public void   update(Long id, MemberUpdateDto updateParam) {
+        memberMapper.update(id, updateParam);
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return memberMapper.findById(id);
+    }
+
+    @Override
+    public List<Member> findAll(MemberSearchC cond) {
+        return memberMapper.findAll(cond);
+    }
+}
