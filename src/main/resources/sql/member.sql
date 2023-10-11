da758f2eac1636a5654aafb88b5f2bad665ecdb1
gyusikbbang <135415038+gyusikbbang@users.noreply.github.com>
2023-10-11 13:15:21 +0900

    2023-10-11 백엔드 커밋

--- src/main/resources/sql/member.sql
+++ src/main/resources/sql/member.sql
@@ -3,7 +3,7 @@ create table member
 --     /*회원테이블 * /
     id     INT AUTO_INCREMENT PRIMARY KEY,
     userid varchar(40),
-    paasword varchar(40),
+    password varchar(40),
 
     name   varchar(40),
     email  varchar(40)
